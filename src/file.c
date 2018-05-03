c325d7c32dad7a8e6c0f29271913773b1ea68e30
MYQ <1275121799@qq.com>
2018-05-03 19:42:21 +0800

    修改了一下

--- src/file.c
+++ src/file.c
@@ -24,34 +24,33 @@ int change_from_char_to_num() {
 
 void read_from_console(order_t *test_order) {
 	char ch;
-	int i;                 /*循环变量*/
-	int a;
+	int index;                 /*循环变量*/
+	int num_of_precedure;
 	scanf("%d", &(test_order->num_of_jobs));       /*输入产品参数*/
 	scanf("%d", &(test_order->num_of_machines));  /*输入机器数目*/
 	test_order->operations = (operation_t**)malloc(sizeof(operation_t*) * test_order->num_of_jobs);  //为指针数组动态分配空间 
-	for (i = 0; i < test_order->num_of_jobs; i++) {
-		test_order->operations[i] = (operation_t*)malloc(sizeof(operation_t) * 10);  //为数组中的元素分配空间 
-		if (test_order->operations[i] == NULL) {
+	for (index = 0; index < test_order->num_of_jobs; index++) {
+		test_order->operations[index] = (operation_t*)malloc(sizeof(operation_t) * 10);  //为数组中的元素分配空间 
+		if (test_order->operations[index] == NULL) {
 			printf("分配失败\n");
 		}
 	}
-	for (i = 0; i < test_order->num_of_jobs; i++) {
-		a = 0;
-		int b;
-		scanf("%d", &b);
+	for (index = 0; index < test_order->num_of_jobs; index++) {
+		num_of_precedure = 0;
+		scanf("%d", &((test_order->operations[index][num_of_precedure]).period));   //产品序号 
 		ch = getchar();
 		while (1) {
 			if (ch == '(') {
-				(test_order->operations[i][a]).period = change_from_char_to_num(getchar());
-				(test_order->operations[i][a]).machine = change_from_char_to_num(getchar());
-				a++;
-				ch = getchar();
+				(test_order->operations[index][num_of_precedure]).period = change_from_char_to_num(getchar());
+				(test_order->operations[index][num_of_precedure]).machine = change_from_char_to_num(getchar());
+				num_of_precedure++;
 			}
 			if (ch == '\n') {
 				break;
 			}
 			ch = getchar();
 		}
+		printf("\n");
 	}
 }
 
@@ -82,13 +81,13 @@ void read_from_file(order_t *input_order, const char *path) {
 				(input_order->operations[index][num_of_precedure]).period = file_change_from_char_to_num(fp);
 				(input_order->operations[index][num_of_precedure]).machine = file_change_from_char_to_num(fp);
 				num_of_precedure++;
-				ch = fgetc(fp);
 			}
 			if (ch == '\n') {
 				break;
 			}
 			ch = fgetc(fp);
 		}
+		printf("\n");
 	}
 	fclose(fp);
 }
@@ -96,9 +95,14 @@ void read_from_file(order_t *input_order, const char *path) {
 int file_change_from_char_to_num(FILE *fp) {
 	char ch = fgetc(fp);
 	int num = 0;
-	while (ch >= '0' && ch <= '9') {
-		num = num * 10 + (ch - '0');
+	if (ch > '9' || ch < '0') {
 		ch = fgetc(fp);
+	}
+	if (ch >= '0' && ch <= '9') {
+		while (ch >= '0' && ch <= '9') {
+			num = num * 10 + (ch - '0');
+			ch = fgetc(fp);
+		}
 	}
 	return num;
 }
